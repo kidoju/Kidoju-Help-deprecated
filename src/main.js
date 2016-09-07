87f1d216244355c74c8d60ebbc5de573b74fa349
jlchereau <jlchereau@msn.com>
2016-09-07 12:28:34 +0100

    New build

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 require('../web/viewer.css');
-require('../web/compatibility.js'); // require('pdfjs-dist/web/compatibility.js');
+require('../web/compatibility.js');
+// require('pdfjs-dist/web/compatibility.js');
 require('../web/l10n.js');
-window.pdfjsDistBuildPdf = require('../build/pdf.js'); // require('pdfjs-dist/web/pdf.js');
+window.pdfjsDistBuildPdf = require('../build/pdf.js');
+// window.pdfjsDistBuildPdf = require('pdfjs-dist/build/pdf.js');
 // require('../web/debugger.js');
 require('./viewer.js');
