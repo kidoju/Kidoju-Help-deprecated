7b8b804de4cb51448195cf0596f724ef7a4a4d7c
jlchereau <jlchereau@msn.com>
2017-02-10 17:20:23 +0000

    Bumped version to 0.2.87 after new production build.

--- src/viewer.js
+++ src/viewer.js
@@ -16,7 +16,7 @@
 
 'use strict';
 
-var DEFAULT_URL = '../docs/user-guide.en.pdf?v=0.2.86';
+var DEFAULT_URL = '../docs/user-guide.en.pdf?v=0.2.87';
 
   var pdfjsWebLibs = {
     pdfjsWebPDFJS: window.pdfjsDistBuildPdf
@@ -6952,7 +6952,7 @@ var DISABLE_AUTO_FETCH_LOADING_BAR_TIMEOUT = 5000;
 
 function configure(PDFJS) {
   PDFJS.imageResourcesPath = './images/';
-  PDFJS.workerSrc = './pdf.worker.bundle.js?v=0.2.86';
+  PDFJS.workerSrc = './pdf.worker.bundle.js?v=0.2.87';
   PDFJS.cMapUrl = './cmaps/';
   PDFJS.cMapPacked = true;
 }
